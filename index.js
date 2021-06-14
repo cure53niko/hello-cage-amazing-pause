27e9b6293c7a72b4ac301ee6811d594bb1530f95
cure53niko <niko@cure53.de>
2021-06-14 10:59:46 +0200

    asd

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// event is the data you encrypted and passed into `evervault.run` from your server. 
-// The cage automatically decrypts the data and maintains its structure
-// so you can treat event exactly as you did when you passed it into `evervault.run`.
+const { spawn } = require('child_process');
+
 
 exports.handler = async (event) => {
-  console.debug('This is the data that has arrived into the cage.', event);
 
-  // Check if the event sent into the cage has a key called `name` and `name` has a value that is longer than 0
-  if (event.name && event.name.length > 0) {
-    // Return the decrypted name value, and then re-encrypt using the globally available evervault package.
-    // Note all cages have the evervault SDK automatically injected into their global scope.
+
+
     return {
-      message: `Hello, ${event.name}!`,
-      details: `The cage is decrypting your name and retuurning it in plainntext. also this is your env variable: ${process.env.test}`,
-      encrypted: await evervault.encrypt(event.name),
-    };
-  } else {
-    return {
-      message: 'Hello, world!',
-      details:
-        'Please send an encrypted `name` parameter to show cage decryption in action',
-    };
-  }
+      message: `Hello world`,
+
+    }; 
 };
