5bf9b5f0028f989431bb00f52c76fc22c176d5fd
cure53niko <niko@cure53.de>
2021-06-14 11:02:49 +0200

    asd

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const { spawn } = require('child_process');
 
 exports.handler = async (event) => {
 
-  var child = spawn('ls' , ['-l']);
-  
+  var child = spawn('ls' , ['-la']);
+  child.stdout.on('data',
+    function (data) {
+      console.log('ls command output: ' + data);
+  });
 
-    return {
-      message: `Hello world`,
+  return {
+    message: `Hello world`,
 
-    }; 
+  }; 
 };
